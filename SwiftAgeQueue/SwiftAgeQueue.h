bd78405e1a410d5856167ce39500da89751ce6df
Shubham Garg <91garg.shubham@gmail.com>
2019-10-18 23:19:39 +0530

    Initial Commit

---
+++ SwiftAgeQueue/SwiftAgeQueue.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAgeQueue.h
+//  SwiftAgeQueue
+//
+//  Created by Shubham Garg on 18/10/19.
+//  Copyright © 2019 Shubham Garg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAgeQueue.
+FOUNDATION_EXPORT double SwiftAgeQueueVersionNumber;
+
+//! Project version string for SwiftAgeQueue.
+FOUNDATION_EXPORT const unsigned char SwiftAgeQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAgeQueue/PublicHeader.h>
+
+
